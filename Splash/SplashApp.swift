cb70f17e7565a1a10b44c969dea3a8ce87808ffa
Kevin Earls <kevinearls@mac.com>
2024-04-08 17:35:13 +0200

    Initial Commit

---
+++ Splash/SplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashApp.swift
+//  Splash
+//
+//  Created by Kevin Earls on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
